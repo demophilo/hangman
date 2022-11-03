5b18bd598f28cb178925ab51659aab2abcbcbba4
Karl Linek <karl@linek.at>
2022-11-03 23:58:47 +0100

    picture for a win changed

--- index.js
+++ index.js
@@ -113,16 +113,15 @@ while ( won === undefined) {
 
 if (won === true) {
   console.log("\n".repeat(50));
-  console.log("  O\n" +
-    " /|\\\n" +
-    " / \\\n" +
-    "=========")
-  console.log("Won!");
+  console.log("    O\n" +
+    "   /|\\\n" +
+    "   / \\\n" +
+    "=========" +
+    "   Won!")
 } else if (won === false || won === undefined) {
   console.log("\n".repeat(50));
 
-  console.log(HANGMAN_PICS.HANGMAN_PICS[6]);
-  console.log("Lost");
+  console.log(HANGMAN_PICS.HANGMAN_PICS[6] + "  Lost");
 }
 
 
